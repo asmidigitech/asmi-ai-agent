486f0f52231f73f9915301def21f773dead9b885
asmidigitech <56154746+asmidigitech@users.noreply.github.com>
2026-04-15 21:52:19 +0530

    Create __manifest__.py

---
+++ ai-voice-agent/__manifest__.py
@@ -0,0 +1,10 @@
+{
+    'name': 'AI Voice Agent',
+    'version': '1.0',
+    'depends': ['crm', 'web'],
+    'data': [
+        # your xml files here
+    ],
+    'installable': True,
+    'application': False,
+}
