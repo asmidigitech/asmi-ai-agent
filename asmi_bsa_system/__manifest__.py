76d3501965413e35a8ac8ecbee9764510bec8bdf
asmidigitech <56154746+asmidigitech@users.noreply.github.com>
2026-04-15 22:08:07 +0530

    Update __manifest__.py

--- asmi_bsa_system/__manifest__.py
+++ asmi_bsa_system/__manifest__.py
@@ -1,1 +1,11 @@
-
+{
+    'name': 'Asmi BSA System',
+    'version': '1.0',
+    'depends': ['crm', 'web'],
+    'data': [
+        'report/report_action.xml',
+        'report/business_score_report.xml',
+    ],
+    'installable': True,
+    'application': False,
+}
